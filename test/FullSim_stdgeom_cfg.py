d3d1842481bb2b120439946ce93c038aef0e1a07
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2009-01-13 23:10:22 +0000

    Normal full simulation job py file with standard geometry

---
+++ test/FullSim_stdgeom_cfg.py
@@ -0,0 +1,100 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Fullsim")
+
+process.load("Configuration.StandardSequences.Services_cff")
+
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+process.load("Configuration.StandardSequences.FakeConditions_cff")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+# this config frament brings you the generator information
+process.load("Configuration.StandardSequences.Generator_cff")
+
+# this config frament brings you 3 steps of the detector simulation:
+# -- vertex smearing (IR modeling)
+# -- G4-based hit level detector simulation
+# -- digitization (electronics readout modeling)
+# it returns 2 sequences : 
+# -- psim (vtx smearing + G4 sim)
+# -- pdigi (digitization in all subsystems, i.e. tracker=pix+sistrips,
+#           cal=ecal+ecal-0-suppression+hcal), muon=csc+dt+rpc)
+#
+process.load("Configuration.StandardSequences.Simulation_cff")
+
+# please note the IMPORTANT: 
+# in order to operate Digis, one needs to include Mixing module 
+# (pileup modeling), at least in the 0-pileup mode
+#
+# There're 3 possible configurations of the Mixing module :
+# no-pileup, low luminosity pileup, and high luminosity pileup
+#
+# they come, respectively, through the 3 config fragments below
+#
+# *each* config returns label "mix"; thus you canNOT have them
+# all together in the same configuration, but only one !!!
+#
+process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
+
+#include "Configuration/StandardSequences/data/MixingLowLumiPileUp.cff" 
+#include "Configuration/StandardSequences/data/MixingHighLumiPileUp.cff" 
+process.load("Configuration.StandardSequences.L1Emulator_cff")
+
+process.load("Configuration.StandardSequences.DigiToRaw_cff")
+
+process.load("Configuration.StandardSequences.RawToDigi_cff")
+
+process.load("Configuration.StandardSequences.VtxSmearedBetafuncEarlyCollision_cff")
+
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+
+# Event output
+process.load("Configuration.EventContent.EventContent_cff")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+process.load("FastSimulation/Configuration/FlatPtMuonGun_cfi")
+# replace FlatRandomPtGunSource.PGunParameters.PartID={13}
+process.FlatRandomPtGunSource.PGunParameters.MinPt = 50.0
+process.FlatRandomPtGunSource.PGunParameters.MaxPt = 50.0
+process.FlatRandomPtGunSource.PGunParameters.MinEta = -2.5
+process.FlatRandomPtGunSource.PGunParameters.MaxEta = 2.5
+
+process.FEVT = cms.OutputModule("PoolOutputModule",
+    process.FEVTSIMEventContent,
+    fileName = cms.untracked.string('/uscms_data/d1/cheung/slhc/testfull_muon_50GeV.root')
+)
+
+process.load("Validation.RecoTrack.cutsTPEffic_cfi")
+process.load("Validation.RecoTrack.cutsTPFake_cfi")
+
+process.load("SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi")
+process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
+
+process.load("Validation.RecoTrack.MultiTrackValidator_cff")
+process.multiTrackValidator.label = ['generalTracks']
+process.multiTrackValidator.associators = ['TrackAssociatorByHits']
+process.multiTrackValidator.UseAssociators = True
+process.multiTrackValidator.outputFile = "validfull_muon_50GeV.root"
+
+process.Timing =  cms.Service("Timing")
+
+# need validation packages
+
+process.p0 = cms.Path(process.pgen)
+process.p1 = cms.Path(process.psim)
+process.p2 = cms.Path(process.pdigi)
+process.p3 = cms.Path(process.L1Emulator)
+process.p4 = cms.Path(process.DigiToRaw)
+process.p5 = cms.Path(process.RawToDigi)
+process.p6 = cms.Path(process.reconstruction)
+process.p7 = cms.Path(process.cutsTPEffic*process.cutsTPFake*process.multiTrackValidator)
+#process.p7 = cms.Path(process.trackingParticles*process.cutsTPEffic*process.cutsTPFake*process.multiTrackValidator)
+process.outpath = cms.EndPath(process.FEVT)
+process.schedule = cms.Schedule(process.p0,process.p1,process.p2,process.p3,process.p4,process.p5,process.p6,process.p7,process.outpath)
