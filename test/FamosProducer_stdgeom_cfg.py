9031c7dcebc25a057bccc19dc0440e087225a7d4
Harry Cheung <sha1-58e66f5ba2315a6f9cad0b2b2d41b73a4257a796@cern.ch>
2009-01-22 21:11:33 +0000

    using the standard fastsim random number generator seeds with the additional seeds needed for digis (more compact form)

--- test/FamosProducer_stdgeom_cfg.py
+++ test/FamosProducer_stdgeom_cfg.py
@@ -4,104 +4,17 @@ process = cms.Process("Fastsimwdigi")
 
 # Number of events to be generated
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(100)
 )
 
 # Include the RandomNumberGeneratorService definition
-#process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
-process.RandomNumberGeneratorService = cms.Service(
-
-    "RandomNumberGeneratorService",
-
-    # To save the status of the last event (useful for crashes)
-    # Just give a name to the file you want the status to be saved
-    # otherwise just put saveFileName = ""
-    saveFileName = cms.untracked.string(''),
-
-    # To restore the status of the last event, just un-comment the
-    # following line (and comment the saveFileName line!)
-    # restoreFileName = cms.string('RandomEngineState.log'),
-
-    # To reproduce events using the RandomEngineStateProducer (source
-    # excluded), comment the sourceSeed definition, and un-comment 
-    # the restoreStateLabel
-    # restoreStateLabel = cms.string('randomEngineStateProducer'),
-
-    # This is to initialize the random engine of the source
-    theSource = cms.PSet(
-        initialSeed = cms.untracked.uint32(123456789),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    # This is to initialize the random engines used for  Famos
-    VtxSmeared = cms.PSet(
-        initialSeed = cms.untracked.uint32(123456789),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-
-    famosPileUp = cms.PSet(
-        initialSeed = cms.untracked.uint32(918273),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    famosSimHits = cms.PSet(
-        initialSeed = cms.untracked.uint32(13579),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    siTrackerGaussianSmearingRecHits = cms.PSet(
-        initialSeed = cms.untracked.uint32(24680),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    caloRecHits = cms.PSet(
-        initialSeed = cms.untracked.uint32(654321),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    paramMuons = cms.PSet(
-        initialSeed = cms.untracked.uint32(54525),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    l1ParamMuons = cms.PSet(
-        initialSeed = cms.untracked.uint32(6453209),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    MuonSimHits = cms.PSet(
-        initialSeed = cms.untracked.uint32(987346),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    simMuonRPCDigis = cms.PSet(
-        initialSeed = cms.untracked.uint32(524964),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    simMuonCSCDigis = cms.PSet(
-        initialSeed = cms.untracked.uint32(525432),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-    simMuonDTDigis = cms.PSet(
-        initialSeed = cms.untracked.uint32(67673876),
-        engineName = cms.untracked.string('TRandom3')
-    ),
-
-   simSiStripDigis = cms.PSet(
+process.load("FastSimulation.Configuration.RandomServiceInitialization_cff")
+process.RandomNumberGeneratorService.simSiStripDigis = cms.PSet(
       initialSeed = cms.untracked.uint32(1234567),
-      engineName = cms.untracked.string('HepJamesRandom')
-   ),
-   simSiPixelDigis = cms.PSet(
+      engineName = cms.untracked.string('HepJamesRandom'))
+process.RandomNumberGeneratorService.simSiPixelDigis = cms.PSet(
       initialSeed = cms.untracked.uint32(1234567),
-      engineName = cms.untracked.string('HepJamesRandom')
-   )
-)
-
-randomEngineStateProducer = cms.EDProducer("RandomEngineStateProducer")
-
+      engineName = cms.untracked.string('HepJamesRandom'))
 
 # Generate H -> ZZ -> l+l- l'+l'- (l,l'=e or mu), with mH=180GeV/c2
 #  process.load("FastSimulation.Configuration.HZZllll_cfi")
@@ -122,8 +35,6 @@ process.FlatRandomPtGunSource.PGunParameters.MaxEta = 2.5
 # Generate di-electrons with pT=35 GeV
 # process.load("FastSimulation/Configuration/DiElectrons_cfi")
 
-# from std full sim (eventsetup for digis?)
-#process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FakeConditions_cff")
 
 # Famos sequences (fake conditions)
@@ -144,10 +55,10 @@ process.famosSimHits.SimulateTracking = True
 # Tracker Digis (Pixel + SiStrips)
 # returns sequence "trDigi"
 #
-#process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
 process.load("SimTracker.Configuration.SimTracker_cff")
 process.simSiPixelDigis.ROUList =  ['famosSimHitsTrackerHits']
 process.simSiPixelDigis.MissCalibrate = False
+process.simSiPixelDigis.AddPixelInefficiency = -1
 process.simSiStripDigis.ROUList =  ['famosSimHitsTrackerHits']
 
 process.load("Configuration.StandardSequences.DigiToRaw_cff")
@@ -165,8 +76,6 @@ process.load("Validation.RecoTrack.cutsTPFake_cfi")
 
 process.load("SimTracker.TrackAssociation.TrackAssociatorByChi2_cfi")
 process.load("SimTracker.TrackAssociation.TrackAssociatorByHits_cfi")
-#process.TrackAssociatorByHits.associateStrip = False
-#process.TrackAssociatorByHits.associatePixel = False
 process.TrackAssociatorByHits.ROUList = ['famosSimHitsTrackerHits']
 
 process.load("Validation.RecoTrack.MultiTrackValidator_cff")
@@ -175,7 +84,7 @@ process.multiTrackValidator.associators = ['TrackAssociatorByHits']
 process.multiTrackValidator.UseAssociators = True
 process.multiTrackValidator.outputFile = "valid_muon_50GeV.root"
 
-# To write out events (not need: FastSimulation _is_ fast!)
+# To write out events
 process.o1 = cms.OutputModule(
     "PoolOutputModule",
     outputCommands = cms.untracked.vstring('keep *',
@@ -193,7 +102,6 @@ process.MessageLogger.destinations = cms.untracked.vstring("detailedInfo_mu50.tx
 process.options = cms.untracked.PSet( Rethrow = cms.untracked.vstring('ProductNotFound') )
 
 # Famos with tracks
-#process.p1 = cms.Path(process.famosWithTrackerHits*process.trackingParticles)
 process.p1 = cms.Path(process.famosWithTrackerHits)
 process.p2 = cms.Path(process.trDigi)
 process.p3 = cms.Path(process.siPixelRawData*process.SiStripDigiToRaw*process.rawDataCollector)
@@ -201,7 +109,4 @@ process.p4 = cms.Path(process.siPixelDigis*process.SiStripRawToDigis)
 process.p5 = cms.Path(process.trackerlocalreco)
 process.p6 = cms.Path(process.offlineBeamSpot+process.recopixelvertexing*process.ckftracks)
 process.p7 = cms.Path(process.trackingParticles*process.cutsTPEffic*process.cutsTPFake*process.multiTrackValidator)
-#process.p7 = cms.Path(process.cutsTPEffic*process.cutsTPFake*process.multiTrackValidator)
-#process.schedule = cms.Schedule(process.p1,process.p2,process.outpath)
-#process.schedule = cms.Schedule(process.p1,process.p2,process.p3,process.p4,process.outpath)
 process.schedule = cms.Schedule(process.p1,process.p2,process.p3,process.p4,process.p5,process.p6,process.p7,process.outpath)
