f6ac73e6b8cc0e1d0ffd3f4f1f0079189c8793a2
Eric Charles Brownson <sha1-bcdc6d153210b6e6996f670d35bc71a14fc4e0d9@cern.ch>
2010-10-05 18:09:33 +0000

    Updated to use new python files

--- test/Muon_Fullsim_Phase1_cfg.py
+++ test/Muon_Fullsim_Phase1_cfg.py
@@ -1,11 +1,12 @@
 # Auto generated configuration file
 # using: 
-# Revision: 1.172.2.5 
+# Revision: 1.222 
 # Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: Configuration/Generator/python/SingleMuPt10_cfi.py -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,L1Reco -n 100 --conditions DESIGN_36_V10::All --datatier GEN-SIM-RAW --eventcontent RAWSIM --beamspot Gauss --python_filename Muon_Fullsim_cfg.py --no_exec
+# with command line options: Configuration/Generator/python/SingleMuPt10_cfi.py -s GEN,SIM,DIGI,L1 -n 10 --conditions DESIGN_36_V10::All --datatier GEN-SIM --eventcontent FEVTDEBUG --beamspot Gauss --python_filename Muon_Fullsim_Phase1_cfg.py --geometry Extended --SLHC R39F16 --no_exec
+# Then Altered to get 4-Muons, 0.9-50 GeV
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process('HLT')
+process = cms.Process('L1')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
@@ -19,21 +20,17 @@ process.load('Configuration.StandardSequences.VtxSmearedGauss_cff')
 process.load('Configuration.StandardSequences.Sim_cff')
 process.load('Configuration.StandardSequences.Digi_cff')
 process.load('Configuration.StandardSequences.SimL1Emulator_cff')
-process.load('Configuration.StandardSequences.DigiToRaw_cff')
-process.load('HLTrigger.Configuration.HLT_1E31_cff')
-process.load('Configuration.StandardSequences.RawToDigi_cff')
-process.load('Configuration.StandardSequences.L1Reco_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 process.load('Configuration.EventContent.EventContent_cff')
 
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.2 $'),
-    annotation = cms.untracked.string('Configuration/Generator/python/SingleMuPt10_cfi.py Altered nevts:100'),
+    version = cms.untracked.string('$Revision: 1.222 $'),
+    annotation = cms.untracked.string('Configuration/Generator/python/SingleMuPt10_cfi.py nevts:10'),
     name = cms.untracked.string('PyReleaseValidation')
 )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(10)
 )
 process.options = cms.untracked.PSet(
 
@@ -42,14 +39,14 @@ process.options = cms.untracked.PSet(
 process.source = cms.Source("EmptySource")
 
 # Output definition
-process.output = cms.OutputModule("PoolOutputModule",
+
+process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
-    outputCommands = process.RAWSIMEventContent.outputCommands,
-    fileName = cms.untracked.string('QuadMuPt10_cfi_py_GEN_SIM_DIGI_L1_USER.root'),
+    outputCommands = process.FEVTDEBUGEventContent.outputCommands,
+    fileName = cms.untracked.string('FourMu_GEN_SIM_DIGI_L1.root'),
     dataset = cms.untracked.PSet(
-        # Adding USER to data tier to scare off non-SLHC people who may try to use it
-        dataTier = cms.untracked.string('GEN-SIM-USER'),
-        filterName = cms.untracked.string('')
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
     ),
     SelectEvents = cms.untracked.PSet(
         SelectEvents = cms.vstring('generation_step')
@@ -63,7 +60,7 @@ process.GlobalTag.globaltag = 'DESIGN_36_V10::All'
 process.generator = cms.EDProducer("FlatRandomPtGunProducer",
     PGunParameters = cms.PSet(
         MaxPt = cms.double(50.0),
-        MinPt = cms.double(0.9),
+        MinPt = cms.double(0.90),
         PartID = cms.vint32(-13,-13),
         MaxEta = cms.double(2.5),
         MaxPhi = cms.double(3.14159265359),
@@ -71,117 +68,27 @@ process.generator = cms.EDProducer("FlatRandomPtGunProducer",
         MinPhi = cms.double(-3.14159265359)
     ),
     Verbosity = cms.untracked.int32(0),
-    psethack = cms.string('Quad mu pt 0.9-50'),
+    psethack = cms.string('Four mu pt 0.9 to 50'),
     AddAntiParticle = cms.bool(True),
     firstRun = cms.untracked.uint32(1)
 )
 
-### PhaseI Geometry and modifications ###############################################
-process.load("SLHCUpgradeSimulations.Geometry.PhaseI_cmsSimIdealGeometryXML_cff")
-process.Timing =  cms.Service("Timing")
-from SimTracker.Configuration.SimTracker_EventContent_cff import *
-from SimGeneral.Configuration.SimGeneral_EventContent_cff import *
-
-process.RAWSIMEventContent.outputCommands.extend(SimGeneralFEVTDEBUG.outputCommands)
-process.RAWSIMEventContent.outputCommands.extend(SimTrackerFEVTDEBUG.outputCommands)
-
-process.siPixelFakeGainOfflineESSource = cms.ESSource("SiPixelFakeGainOfflineESSource",
-  file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/EmptyPixelSkimmedGeometry_phase1.txt')
-)
-process.es_prefer_fake_gain = cms.ESPrefer("SiPixelFakeGainOfflineESSource","siPixelFakeGainOfflineESSource")
-
-process.siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
-  file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/PixelSkimmedGeometry_phase1.txt')
-)
-process.es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
-
-process.load("CalibTracker.SiStripESProducers.fake.SiStripNoisesFakeESSource_cfi")
-# Next two lines are antiquated in form, and we use the proper values by default
-#process.SiStripNoisesGenerator.NoiseStripLengthSlope=51. #dec mode
-#process.SiStripNoisesGenerator.NoiseStripLengthQuote=630.
-
-process.siStripNoisesFakeESSource  = cms.ESSource("SiStripNoisesFakeESSource")
-process.es_prefer_fake_strip_noise = cms.ESPrefer("SiStripNoisesFakeESSource",
-                                                  "siStripNoisesFakeESSource")
-
-process.load("CalibTracker.SiStripESProducers.fake.SiStripQualityFakeESSource_cfi")
-
-process.siStripQualityFakeESSource  = cms.ESSource("SiStripQualityFakeESSource")
-process.es_prefer_fake_strip_quality = cms.ESPrefer("SiStripQualityFakeESSource",
-                                                     "siStripQualityFakeESSource")
-
-process.load("CalibTracker.SiStripESProducers.fake.SiStripPedestalsFakeESSource_cfi")
-
-process.siStripPedestalsFakeESSource  = cms.ESSource("SiStripPedestalsFakeESSource")
-process.es_prefer_fake_strip_pedestal = cms.ESPrefer("SiStripPedestalsFakeESSource",
-                                                     "siStripPedestalsFakeESSource")
-
-process.load("CalibTracker.SiStripESProducers.fake.SiStripLorentzAngleFakeESSource_cfi")
-
-process.siStripLorentzAngleFakeESSource  = cms.ESSource("SiStripLorentzAngleFakeESSource")
-process.es_prefer_fake_strip_LA = cms.ESPrefer("SiStripLorentzAngleFakeESSource",
-                                               "siStripLorentzAngleFakeESSource")
-
-process.siStripLorentzAngleSimFakeESSource  = cms.ESSource("SiStripLorentzAngleSimFakeESSource")
-process.es_prefer_fake_strip_LA_sim = cms.ESPrefer("SiStripLorentzAngleSimFakeESSource",
-                                                   "siStripLorentzAngleSimFakeESSource")
-
-process.load("CalibTracker.SiStripESProducers.fake.SiStripApvGainFakeESSource_cfi")
-process.SiStripApvGainGenerator.MeanGain=1.0
-process.SiStripApvGainGenerator.SigmaGain=0.0
-process.SiStripApvGainGenerator.genMode = cms.string("default")
-process.myStripApvGainFakeESSource = cms.ESSource("SiStripApvGainFakeESSource")
-process.es_prefer_myStripApvGainFakeESSource  = cms.ESPrefer("SiStripApvGainFakeESSource",
-                                                  "myStripApvGainFakeESSource")
-
-process.myStripApvGainSimFakeESSource  = cms.ESSource("SiStripApvGainSimFakeESSource")
-process.es_prefer_myStripApvGainSimFakeESSource = cms.ESPrefer("SiStripApvGainSimFakeESSource",
-                                                               "myStripApvGainSimFakeESSource")
-
-process.load("CalibTracker.SiStripESProducers.fake.SiStripThresholdFakeESSource_cfi")
-
-process.siStripThresholdFakeESSource  = cms.ESSource("SiStripThresholdFakeESSource")
-process.es_prefer_fake_strip_threshold = cms.ESPrefer("SiStripThresholdFakeESSource",
-                                                     "siStripThresholdFakeESSource")
-
-process.TrackerDigiGeometryESModule.applyAlignment = False
-process.simSiPixelDigis.MissCalibrate = False
-process.simSiPixelDigis.LorentzAngle_DB = False
-process.simSiPixelDigis.killModules = False
-process.simSiPixelDigis.useDB = False
-process.simSiPixelDigis.DeadModules_DB = False
-process.simSiPixelDigis.NumPixelBarrel = cms.int32(4)
-process.simSiPixelDigis.NumPixelEndcap = cms.int32(3)
-process.simSiPixelDigis.AddPixelInefficiency = -1
-
-process.MeasurementTracker.inactiveStripDetectorLabels = cms.VInputTag()
-process.MeasurementTracker.UseStripModuleQualityDB     = cms.bool(False)
-process.MeasurementTracker.UseStripAPVFiberQualityDB   = cms.bool(False)
-
-process.mergedtruth.volumeRadius = cms.double(100.0)
-process.mergedtruth.volumeZ = cms.double(900.0)
-process.mergedtruth.discardOutVolume = cms.bool(True)
-
-
-### back to standard job commands ##################################################
+# SLHC Additions ##########
+process.load("SLHCUpgradeSimulations.Geometry.PhaseI_cmsSimIdealGeometryXML_R39F16_cff")
+process.load("SLHCUpgradeSimulations.Geometry.fakeConditions_Phase1_cff")
+# Back To Normal ##########
 
 # Path and EndPath definitions
 process.generation_step = cms.Path(process.pgen)
 process.simulation_step = cms.Path(process.psim)
 process.digitisation_step = cms.Path(process.pdigi)
 process.L1simulation_step = cms.Path(process.SimL1Emulator)
-process.digi2raw_step = cms.Path(process.DigiToRaw)
-process.raw2digi_step = cms.Path(process.RawToDigi)
-process.L1Reco_step = cms.Path(process.L1Reco)
 process.endjob_step = cms.Path(process.endOfProcess)
-process.out_step = cms.EndPath(process.output)
+process.FEVTDEBUGoutput_step = cms.EndPath(process.FEVTDEBUGoutput)
 
 # Schedule definition
-process.schedule = cms.Schedule(process.generation_step,process.simulation_step,process.digitisation_step,process.L1simulation_step)
-#process.schedule = cms.Schedule(process.generation_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
-#process.schedule.extend(process.HLTSchedule)
-#process.schedule.extend([process.raw2digi_step,process.L1Reco_step,process.endjob_step,process.out_step])
-process.schedule.extend([process.endjob_step,process.out_step])
-# special treatment in case of production filter sequence  
+process.schedule = cms.Schedule(process.generation_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.endjob_step,process.FEVTDEBUGoutput_step)
+
+# special treatment in case of production filter sequence
 for path in process.paths: 
     getattr(process,path)._seq = process.generator*getattr(process,path)._seq
